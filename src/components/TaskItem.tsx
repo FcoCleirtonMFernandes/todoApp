6943c83222ed22f4b244fdcc84a86e264b9616b7
Cleirton <79455186+FcoCleirtonMFernandes@users.noreply.github.com>
2022-10-04 09:53:40 -0300

    Create TaskItem.tsx
    Criando o componente TaskItem

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { 
+    TouchableOpacity,
+    View, 
+    Text, 
+    StyleSheet,
+    Image
+  } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import trashIcon from '../assets/icons/trash/trash.png'
+import { EditTaskArgs } from '../pages/Home';
+import { Task } from './TasksList';
+
+interface TasksItemProps {
+    task: Task;
+    toggleTaskDone: (id: number) => void;
+    removeTask: (id: number) => void;
+    editTask: ({ taskId, taskNewTitle }: EditTaskArgs) => void;
+}
+
+export function TaskItem({ task, editTask, removeTask, toggleTaskDone }: TasksItemProps) {
+  return (
+    <>
+        <View>
+            <TouchableOpacity
+                activeOpacity={0.7}
+                style={styles.taskButton}
+                onPress={() => toggleTaskDone(task.id)}
+            >
+            <View 
+                style={task.done ? styles.taskMarkerDone : styles.taskMarker}
+            >
+                { task.done && (
+                <Icon 
+                    name="check"
+                    size={12}
+                    color="#FFF"
+                />
+                )}
+            </View>
+
+            <Text 
+                style={task.done ? styles.taskTextDone : styles.taskText}
+            >
+                {task.title}
+            </Text>
+
+            </TouchableOpacity>
+            
+            <TouchableOpacity
+              style={{ paddingHorizontal: 24 }}
+              //TODO - use onPress (remove task) prop
+              onPress={() => removeTask(task.id)}
+            >
+              <Image source={trashIcon} />
+            </TouchableOpacity>
+        </View>
+    </>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    taskButton: {
+      flex: 1,
+      paddingHorizontal: 24,
+      paddingVertical: 15,
+      marginBottom: 4,
+      borderRadius: 4,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    taskMarker: {
+      height: 16,
+      width: 16,
+      borderRadius: 4,
+      borderWidth: 1,
+      borderColor: '#B2B2B2',
+      marginRight: 15,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    taskText: {
+      color: '#666',
+      fontFamily: 'Inter-Medium'
+    },
+    taskMarkerDone: {
+      height: 16,
+      width: 16,
+      borderRadius: 4,
+      backgroundColor: '#1DB863',
+      marginRight: 15,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    taskTextDone: {
+      color: '#1DB863',
+      textDecorationLine: 'line-through',
+      fontFamily: 'Inter-Medium'
+    }
+  })
